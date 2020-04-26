03c273fb81ef8965d34b066c4ff2714a1d76f2b3
bindu <bindu@Rajeshs-MacBook-Pro.local>
2020-04-26 21:37:27 +0545

    login template

--- run.py
+++ run.py
@@ -11,5 +11,9 @@ def index():
 def signup():
     return render_template('signup.html')
 
+@app.route('/login')
+def login():
+    return render_template('login.html')
+
 if __name__=='__main__':
    app.run()
