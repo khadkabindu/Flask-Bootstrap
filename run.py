42f50575f74378cb8b6eb86283792e6b30ce76a1
bindu <bindu@Rajeshs-MacBook-Pro.local>
2020-04-26 18:14:05 +0545

    sign up form

--- run.py
+++ run.py
@@ -7,5 +7,9 @@ app = Flask(__name__)
 def index():
     return render_template('index.html')
 
+@app.route('/signup')
+def signup():
+    return render_template('signup.html')
+
 if __name__=='__main__':
    app.run()
